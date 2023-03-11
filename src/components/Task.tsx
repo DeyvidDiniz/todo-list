e96087bbbf7f446e0929c9c7019c94ebcd7e53d4
DeyvidDiniz <deyvidatd@hotmail.com>
2023-03-11 17:16:18 -0300

    Criação dos checkbox de tasks

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,49 +1,55 @@
-import { Trash } from 'phosphor-react'
+import { Trash, CheckCircle } from 'phosphor-react'
 import style from './Task.module.css'
+import * as Checkbox from '@radix-ui/react-checkbox';
 
 export function Task(){
+function teste(){
+    console.log('teste')
+}
+
     return(
+
         <div>
 
-        <div className={style.task}>
-            <div className={style.taskContainer}>
-                <input type="checkbox"/>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo beatae velit, eum nulla iure ab sint veritatis sunt maxime enim.</p>
+            <div className={style.task}>
+                <div className={style.taskContainer}>
+                    <label>
+                        <input type="checkbox" /> 
+                        <span></span>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo beatae velit, eum nulla iure ab sint veritatis sunt maxime enim.</p>
+                    </label>
+                </div>
+                <button >
+                    <Trash size={14} />
+                </button>
             </div>
-            <Trash size={14}/>
-        </div>
 
-        <div className={style.task}>
-            <div className={style.taskContainer}>
-                <input type="checkbox"/>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo beatae velit, eum nulla iure ab sint veritatis sunt maxime enim.</p>
+            <div className={style.task}>
+                <div className={style.taskContainer}>
+                    <label>
+                        <input type="checkbox" /> 
+                        <span></span>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo beatae velit, eum nulla iure ab sint veritatis sunt maxime enim.</p>
+                    </label>
+                </div>
+                <button >
+                    <Trash size={14} />
+                </button>
             </div>
-            <Trash size={14}/>
-        </div>
 
-        <div className={style.task}>
-            <div className={style.taskContainer}>
-                <input type="checkbox"/>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo beatae velit, eum nulla iure ab sint veritatis sunt maxime enim.</p>
+            <div className={style.task}>
+                <div className={style.taskContainer}>
+                    <label>
+                        <input type="checkbox" /> 
+                        <span></span>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo beatae velit, eum nulla iure ab sint veritatis sunt maxime enim.</p>
+                    </label>
+                </div>
+                <button >
+                    <Trash size={14} />
+                </button>
             </div>
-            <Trash size={14}/>
-        </div>
 
-        <div className={style.task}>
-            <div className={style.taskContainer}>
-                <input type="checkbox"/>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo beatae velit, eum nulla iure ab sint veritatis sunt maxime enim.</p>
-            </div>
-            <Trash size={14}/>
-        </div>
-
-        <div className={style.task}>
-            <div className={style.taskContainer}>
-                <input type="checkbox"/>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo beatae velit, eum nulla iure ab sint veritatis sunt maxime enim.</p>
-            </div>
-            <Trash size={14}/>
-        </div>
 
         </div>
 
